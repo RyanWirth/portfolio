a01e759230544c437398e641338c63d552759ae0
Ryan Wirth <ryan@ryanwirth.ca>
2020-06-23 16:16:56 -0600

    Add icon-web-developer to images.svg

--- src/components/Skills.svelte
+++ src/components/Skills.svelte
@@ -21,30 +21,49 @@
     }
   }
 
+  h4 {
+    margin: 32px 0 16px 0;
+  }
+
   .skills__container {
     display: flex;
     justify-content: center;
+    margin: 104px 0;
   }
 
   .skills__item {
     flex: 0 340px;
-    margin: 0 20px;
+    margin: 0 16px;
     text-align: center;
+  }
+
+  .skills__item--web-developer svg {
+    width: 128px;
+    height: 93px;
   }
 </style>
 
 <section>
   <h2>Building awesome, one commit at a time.</h2>
   <div class="skills__container">
-    <div class="skills__item">
+    <div class="skills__item skills__item--web-developer">
+      <svg>
+        <use xlink:href="/images.svg#icon-web-developer" />
+      </svg>
       <h4>Paperwork is out</h4>
       <p>Flip through your complete records with the tap of a button.</p>
     </div>
-    <div class="skills__item">
+    <div class="skills__item skills__item--web-developer">
+      <svg>
+        <use xlink:href="/images.svg#icon-web-developer" />
+      </svg>
       <h4>A chronology of care</h4>
       <p>Keep organized with a timeline of your entire health history.</p>
     </div>
-    <div class="skills__item">
+    <div class="skills__item skills__item--web-developer">
+      <svg>
+        <use xlink:href="/images.svg#icon-web-developer" />
+      </svg>
       <h4>You are in control</h4>
       <p>Review and share -- all on your own terms.</p>
     </div>
